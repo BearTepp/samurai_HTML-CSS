3a72edecef8f6cd7c9e3677bedb120006cb9d812
EC2 Default User <ec2-user@ip-172-31-21-130.ap-northeast-1.compute.internal>
2023-03-25 23:50:37 +0000

    first commit

---
+++ class2.rb
@@ -0,0 +1,15 @@
+class Human
+    @@class_name = "Human"
+    
+    def initialize
+        @name = "大泉"
+    end
+    
+    def show()
+        puts @name
+    end
+end
+
+class Hoge end
+    
+    
---
+++ var.rb
@@ -0,0 +1,4 @@
+hello = "Hello, Ruby!"
+num = 42
+puts hello
+puts num
